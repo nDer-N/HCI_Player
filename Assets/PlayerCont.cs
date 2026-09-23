7e1a7621b51b4ee2c588e609bb325c3ea00d3858
CIAC <CIAC@CIAC1-019.academic.mixcoac.upmx.mx>
2026-09-22 19:18:31 -0600

    corrections

--- Assets/PlayerCont.cs
+++ Assets/PlayerCont.cs
@@ -1,31 +1,30 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerCont : MonoBehaviour
 {
     public float velocity = 8f;
     public float Jump = 7f;
-    
+
     private Rigidbody2D rb;
     private bool floor = true;
-
-    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
-        
     }
 
     void Update()
     {
         float movement = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector3(movement*velocity, rb.linearVelocity.y, 0 );
+        rb.linearVelocity = new Vector3(movement * velocity, rb.linearVelocity.y, 0);
 
-          
+        if (floor && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Jump);
+            floor = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,7 +32,6 @@ public class PlayerCont : MonoBehaviour
         if (collision.gameObject.CompareTag("Suelo"))
         {
             floor = true;
-            Debug.Log(floor);
         }
     }
 }
